ae3606388ff2f2a34410a9a4927c12a35339e05c
Donchuks02 <chukwudidavid02@gmail.com>
2024-10-10 18:49:12 +0100

    added a new functionality for managing departments and associating users with multiple departments

--- create_table.py
+++ create_table.py
@@ -2,30 +2,43 @@ import psycopg2
 from DB_config_file import get_config
 
 def create_table():
-  '''
-  create table in database
-  '''
-  queries = (
-    """
-      CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        date_joined VARCHAR(255) NOT NULL
-      )
-    """,
-  )
-  try :
-    config = get_config()
-    with psycopg2.connect(**config) as connection:
-      with connection.cursor() as cursor:
-        #after the connection is established, execute the CREATE TABLE query but first drop the table if it already exists
-        cursor.execute('DROP TABLE IF EXISTS users')
-        for query in queries:
-          cursor.execute(query)
-  except Exception as error:
-    print(error)
+    '''create table in database'''
+    queries = (
+        """
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            date_joined VARCHAR(255) NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE departments (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) UNIQUE NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE user_departments (
+            user_id INT REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+            department_name VARCHAR(255) REFERENCES departments(name) ON UPDATE CASCADE ON DELETE CASCADE,
+            PRIMARY KEY (user_id, department_name)
+        )
+        """,
+    )
 
+    try:
+        config = get_config()
+        with psycopg2.connect(**config) as connection:
+            with connection.cursor() as cursor:
+                # After the connection is established, execute the CREATE TABLE query but first drop the table if it already exists
+                cursor.execute('DROP TABLE IF EXISTS user_departments CASCADE')
+                cursor.execute('DROP TABLE IF EXISTS departments CASCADE')
+                cursor.execute('DROP TABLE IF EXISTS users CASCADE')
+                for query in queries:
+                    cursor.execute(query)
+    except Exception as error:
+        print(error)
 
 if __name__ == '__main__':
-  create_table()
+    create_table()
--- insert_user.py
+++ insert_user.py
@@ -2,8 +2,8 @@ from DB_config_file import get_config
 import psycopg2
 from datetime import datetime
 
-def insert_many_records(users_list):
-  query = "INSERT INTO users (name, email, date_joined) VALUES (%s, %s, %s)"
+def insert_many_users(users_list):
+  query = """INSERT INTO users (name, email, date_joined) VALUES (%s, %s, %s) ON CONFLICT (email) DO NOTHING;"""
   config = get_config()
   try:
     with psycopg2.connect(**config) as connection:
@@ -11,13 +11,45 @@ def insert_many_records(users_list):
         cursor.executemany(query, users_list)
 
       connection.commit()
-
+    print("users inserted successfully")
   except Exception as error:
-    print
+    print(error)
 
+
+def insert_many_departments(departments_list):
+  """Inserts departments into the 'departments' table"""
+  query = """INSERT INTO departments (name) VALUES (%s) ON CONFLICT (name) DO NOTHING;"""
+  config = get_config()
+
+  try:
+    with psycopg2.connect(**config) as connection:
+      with connection.cursor() as cursor:
+        cursor.executemany(query, departments_list)
+
+      connection.commit()
+    print("departments inserted successfully")
+  except Exception as error:
+    print(error)
+
+
+def insert_user_departments(user_department_list):
+  """Inserts a user-department relationship into the 'user_departments' table"""
+  query = """INSERT INTO user_departments (user_id, department_name) VALUES (%s, %s) ON CONFLICT (user_id, department_name) DO NOTHING;"""
+  config = get_config()
+
+  try:
+    with psycopg2.connect(**config) as connection:
+      with connection.cursor() as cursor:
+        cursor.executemany(query, user_department_list)
+
+      connection.commit()
+    print("user-department relationship inserted successfully")
+  except Exception as error:
+    print(error)
 
 if __name__ == "__main__":
-  insert_many_records([
+  # inserting users
+  insert_many_users([
       ('John', 'john@gmail.com', datetime.now(),),
       ('James', 'james@gmail.com', datetime.now(),),
       ('Ada', 'ada@gmail.com', datetime.now(),),
@@ -25,3 +57,35 @@ if __name__ == "__main__":
       ('Cain', 'cain@gmail.com', datetime.now(),),
       ('Paul', 'paul@gmail.com', datetime.now(),),
     ])
+
+  #inserting departments
+  insert_many_departments([
+      ('Engineering',),
+      ('Sales',),
+      ('Marketing',),
+      ('Finance',),
+      ('HR',),
+      ('Legal',),
+      ('Customer service',),
+      ('IT',),
+      ('Admin',),
+      ('Accounting',),
+      ('Public Relations',),
+  ])
+
+
+  #inserting user-department relationships
+  insert_user_departments([
+      (1, 'Engineering',),
+      (1, 'Sales',),
+      (1, 'Marketing',),
+      (2, 'Sales',),
+      (3, 'Engineering',),
+      (3, 'Sales',),
+      (3, 'Marketing',),
+      (4, 'Engineering',),
+      (4, 'Sales',),
+      (5, 'Engineering',),
+      (6, 'Accounting',),
+  ])
+
